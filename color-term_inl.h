8cbef8b9d2632023a9bf3792ea15833b52cb315c
liefswanson <liefs@sfu.ca>
2015-04-08 14:26:31 -0700

    untested header only.

---
+++ color-term_inl.h
@@ -0,0 +1,45 @@
+#ifndef _COLOR-TERM_INL_H_
+#define _COLOR-TERM_INL_H_
+
+const char* term_reset  = "\e[0m";
+
+const char* term_black  = "\e[30m";
+const char* term_red    = "\e[31m";
+const char* term_green  = "\e[32m";
+const char* term_yellow = "\e[33m";
+const char* term_blue   = "\e[34m";
+const char* term_purple = "\e[35m";
+const char* term_cyan   = "\e[36m";
+const char* term_white  = "\e[37m";
+
+const char* term_bg_black  = "\e[40m";
+const char* term_bg_red    = "\e[41m";
+const char* term_bg_green  = "\e[42m";
+const char* term_bg_yellow = "\e[43m";
+const char* term_bg_blue   = "\e[44m";
+const char* term_bg_purple = "\e[45m";
+const char* term_bg_cyan   = "\e[46m";
+const char* term_bg_white  = "\e[47m";
+
+const char* term_bg_light_black  = "\e[100m";
+const char* term_bg_light_red    = "\e[101m";
+const char* term_bg_light_green  = "\e[102m";
+const char* term_bg_light_yellow = "\e[103m";
+const char* term_bg_light_blue   = "\e[104m";
+const char* term_bg_light_purple = "\e[105m";
+const char* term_bg_light_cyan   = "\e[106m";
+const char* term_bg_light_white  = "\e[107m";
+
+const char* term_light_on  = "\e[1m";
+const char* term_light_off = "\e[22m";
+
+const char* term_ital_on   = "\e[3m";
+const char* term_ital_off  = "\e[23m";
+
+const char* term_uline_on  = "\e[4m";
+const char* term_uline_off = "\e[24m";
+
+const char* term_blink_on  = "\e[5m";
+const char* term_blink_off = "\e[25m";
+
+#endif /* _COLOR-TERM_INL_H_ */
